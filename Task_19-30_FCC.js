d3999a0362e4a680f97e88a92d7aad9ac1d269ff
glebyshkin <glebskobla@tut.by>
2021-10-21 02:37:45 +0300

    all tasks 19-30 done

---
+++ Task_19-30_FCC.js
@@ -0,0 +1,59 @@
+//task 19
+var myStr = '<a "href=http://www.example.com" target="_blank">Link</a>';
+
+//task 20
+var myStr="FirstLine\n\t\\SecondLine\nThirdLine";
+
+// task 21
+var myStr = "This is the start. "+"This is the end.";
+
+//task 22
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
+
+// task 23
+var myName = "Hleb";
+var myStr = "Hello, my name is " + myName + ", how are you?";
+
+// task 24
+var someAdjective = "exciting!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// task 25
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
+
+// task 26
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//task 27
+// Setup
+var myStr = "Jello World";
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// task 28
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
+
+//task 29
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+console.log(lastLetterOfLastName);
+
+//task 30
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
