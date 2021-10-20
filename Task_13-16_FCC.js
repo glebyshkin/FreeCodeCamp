6de7e9eb8fad7991b3c5995cdb7dcadce5fdead6
glebyshkin <glebskobla@tut.by>
2021-10-21 00:43:53 +0300

    tasks 13-16 done

---
+++ Task_13-16_FCC.js
@@ -0,0 +1,39 @@
+// task 13
+
+var a = 3;
+var b = 17;
+var c = 12;
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// task 14
+
+var a = 11;
+var b = 9;
+var c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+// task 15
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+// task 16
+
+var a = 48;
+var b = 108;
+var c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
