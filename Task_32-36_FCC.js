e9be0195caacd01bfd254de54c10c35b97d03f24
glebyshkin <glebskobla@tut.by>
2021-10-22 01:22:23 +0300

    tasks 32-36 done!

---
+++ Task_32-36_FCC.js
@@ -0,0 +1,21 @@
+// task 32
+var myArray = ["coconut", 12];
+
+// task 33
+var myArray = [["Mr. Bean", 45], ["Margarett", 37]];
+
+
+// task 34
+var myArray = [50,60,70];
+var myData = myArray[0];
+
+
+//task 35
+// Setup
+var myArray = [18,64,99];
+// Only change code below this line
+myArray[0] = 45;
+
+//task 36
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+var myData = myArray[2][1];
