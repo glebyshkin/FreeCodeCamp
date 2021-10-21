3be9e8f505b441228472c48bd30631b6d97de9d8
glebyshkin <glebskobla@tut.by>
2021-10-22 01:40:17 +0300

    tasks 37-40 done

---
+++ Task_37-40_FCC.js
@@ -0,0 +1,29 @@
+// task 37
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+myArray.push(["dog", 3]);
+
+// task 38
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
+console.log(myArray);
+console.log(removedFromMyArray)
+
+// task 39
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
+console.log(myArray);
+console.log(removedFromMyArray);
+
+// task 40
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+console.log(myArray);
