802eefc7126401fb4d40b2358508da9b2ff3cb9f
glebyshkin <glebskobla@tut.by>
2021-10-22 03:24:58 +0300

    tasks 51-55 done

---
+++ Task_51-55_FCC.js
@@ -0,0 +1,48 @@
+//task 51
+function welcomeToBooleans() {
+    // Only change code below this line
+      return true; // Change this line
+      // Only change code above this line
+  }
+
+  // task 52
+  function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if (wasThatTrue){
+      return "Yes, that was true";
+    }
+    return "No, that was false"
+    // Only change code above this line
+
+  }
+
+  // task 53
+  // Setup
+function testEqual(val) {
+    if (val == 12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+    testEqual(10);
+
+    // task 54
+    // Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+    testStrict(10);
+
+    //task 55
+    // Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+
+  compareEquality(10, "10");
