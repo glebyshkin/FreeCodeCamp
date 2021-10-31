53d0d602547961f2b3914502bd6d08d6226c514a
glebyshkin <glebskobla@tut.by>
2021-10-31 22:38:54 +0300

    done

---
+++ task85.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+  "name": "Hennesey",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Ball","Toys","Parents"]
+
+
+    // Only change code above this line
+  };
